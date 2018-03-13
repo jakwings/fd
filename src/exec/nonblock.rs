21898910a43cbc696c26d694f6c7ccffa0c26c54
J.W <jakwings@gmail.com>
2018-03-13 12:10:08 +0800

    Fix logic about using select(2)

--- src/exec/nonblock.rs
+++ src/exec/nonblock.rs
@@ -2,6 +2,8 @@ use std::io::{self, Read, Write};
 use std::os::unix::io::RawFd;
 use std::sync::Arc;
 use std::sync::atomic::{self, AtomicBool};
+use std::thread;
+use std::time;
 
 use super::nix::Error;
 use super::nix::errno::Errno;
@@ -9,7 +11,7 @@ use super::nix::sys::select;
 use super::nix::sys::time::{TimeVal, TimeValLike};
 
 const BUF_SIZE: usize = 512;
-const INTERVAL: i64 = 500 * 1000; // 500 microseconds
+const INTERVAL: u32 = 500 * 1000; // 500 microseconds
 const MAX_CNT: u32 = 500;
 
 fn loop_counter(counter: &mut u32) {
@@ -20,18 +22,15 @@ fn load_bool(atom: &Arc<AtomicBool>) -> bool {
     atom.load(atomic::Ordering::Relaxed)
 }
 
-pub fn select_read_to_end<R: Read>(
+fn read_to_end<R: Read>(
     atom: &Arc<AtomicBool>,
-    fd: RawFd,
-    reader: &mut R,
+    reader: &mut R, // could be blocking
     content: &mut Vec<u8>,
 ) -> io::Result<Option<usize>> {
     let mut total = 0;
     let mut buffer = [0; BUF_SIZE];
     let mut counter = 0;
-    let mut fdset = select::FdSet::new();
-
-    fdset.insert(fd);
+    let interval = time::Duration::new(0, INTERVAL);
 
     loop {
         if counter >= MAX_CNT && load_bool(&atom) {
@@ -39,37 +38,14 @@ pub fn select_read_to_end<R: Read>(
         } else {
             loop_counter(&mut counter);
         }
-
-        let mut interval = TimeVal::nanoseconds(INTERVAL);
-
-        match select::select(
-            Some(fd + 1),
-            Some(&mut fdset),
-            None,
-            None,
-            Some(&mut interval),
-        ) {
-            Ok(0) => (), // timeout
-            Ok(_) => {
-                if fdset.contains(fd) {
-                    match reader.read(&mut buffer) {
-                        Ok(0) => break,
-                        Ok(size) => total += content.write(&buffer[0..size]).unwrap(),
-                        Err(err) => {
-                            if err.kind() != io::ErrorKind::WouldBlock {
-                                return Err(err);
-                            }
-                        }
-                    }
-                } else {
-                    unreachable!("[Error] unknown bugs about select(2)");
-                }
-            }
-            Err(Error::Sys(Errno::EINTR)) => (), // unavailable
+        match reader.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(size) => total += content.write(&buffer[0..size]).unwrap(),
             Err(err) => {
-                use std::error::Error;
-
-                return Err(io::Error::new(io::ErrorKind::Other, err.description()));
+                if err.kind() != io::ErrorKind::WouldBlock {
+                    return Err(err);
+                }
+                thread::sleep(interval);
             }
         }
     }
@@ -77,18 +53,15 @@ pub fn select_read_to_end<R: Read>(
     Ok(Some(total))
 }
 
-pub fn select_write_all<W: Write>(
+fn write_all<W: Write>(
     atom: &Arc<AtomicBool>,
-    fd: RawFd,
-    writer: &mut W,
+    writer: &mut W, // could be blocking
     content: &Vec<u8>,
 ) -> io::Result<Option<()>> {
     let mut total = 0;
     let length = content.len();
     let mut counter = 0;
-    let mut fdset = select::FdSet::new();
-
-    fdset.insert(fd);
+    let interval = time::Duration::new(0, INTERVAL);
 
     loop {
         if counter >= MAX_CNT && load_bool(&atom) {
@@ -98,7 +71,86 @@ pub fn select_write_all<W: Write>(
         }
 
         let range = total..(BUF_SIZE + total).min(length);
-        let mut interval = TimeVal::nanoseconds(INTERVAL);
+
+        match writer.write(&content[range]) {
+            Ok(0) => break,
+            Ok(size) => total += size,
+            Err(err) => {
+                if err.kind() != io::ErrorKind::WouldBlock {
+                    return Err(err);
+                }
+                thread::sleep(interval);
+            }
+        }
+    }
+
+    Ok(Some(()))
+}
+
+pub fn select_read_to_end<R: Read>(
+    atom: &Arc<AtomicBool>,
+    fd: RawFd,
+    reader: &mut R,
+    content: &mut Vec<u8>,
+) -> io::Result<Option<usize>> {
+    let mut counter = 0;
+    let mut fdset = select::FdSet::new();
+
+    loop {
+        if counter >= MAX_CNT && load_bool(&atom) {
+            return Ok(None);
+        } else {
+            loop_counter(&mut counter);
+        }
+
+        let mut interval = TimeVal::nanoseconds(INTERVAL as i64);
+
+        fdset.insert(fd);
+
+        match select::select(
+            Some(fd + 1),
+            Some(&mut fdset),
+            None,
+            None,
+            Some(&mut interval),
+        ) {
+            Ok(0) => (), // timeout
+            Ok(_) => {
+                if fdset.contains(fd) {
+                    return read_to_end(atom, reader, content);
+                } else {
+                    unreachable!("[Error] unknown bugs about select(2)");
+                }
+            }
+            Err(Error::Sys(Errno::EINTR)) => (),
+            Err(err) => {
+                use std::error::Error;
+
+                return Err(io::Error::new(io::ErrorKind::Other, err.description()));
+            }
+        }
+    }
+}
+
+pub fn select_write_all<W: Write>(
+    atom: &Arc<AtomicBool>,
+    fd: RawFd,
+    writer: &mut W,
+    content: &Vec<u8>,
+) -> io::Result<Option<()>> {
+    let mut counter = 0;
+    let mut fdset = select::FdSet::new();
+
+    loop {
+        if counter >= MAX_CNT && load_bool(&atom) {
+            return Ok(None);
+        } else {
+            loop_counter(&mut counter);
+        }
+
+        let mut interval = TimeVal::nanoseconds(INTERVAL as i64);
+
+        fdset.insert(fd);
 
         match select::select(
             Some(fd + 1),
@@ -110,20 +162,12 @@ pub fn select_write_all<W: Write>(
             Ok(0) => (), // timeout
             Ok(_) => {
                 if fdset.contains(fd) {
-                    match writer.write(&content[range]) {
-                        Ok(0) => break,
-                        Ok(size) => total += size,
-                        Err(err) => {
-                            if err.kind() != io::ErrorKind::WouldBlock {
-                                return Err(err);
-                            }
-                        }
-                    }
+                    return write_all(atom, writer, content);
                 } else {
                     unreachable!("[Error] unknown bugs about select(2)");
                 }
             }
-            Err(Error::Sys(Errno::EINTR)) => (), // unavailable
+            Err(Error::Sys(Errno::EINTR)) => (),
             Err(err) => {
                 use std::error::Error;
 
@@ -131,6 +175,4 @@ pub fn select_write_all<W: Write>(
             }
         }
     }
-
-    Ok(Some(()))
 }

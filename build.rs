a9562facdf037465154bacffec26c376910baf39
J.W <jakwings@gmail.com>
2018-12-03 07:37:29 +0800

    Update build script

--- build.rs
+++ build.rs
@@ -1,11 +1,20 @@
 extern crate clap;
 
+use std::path::PathBuf;
+
 use clap::Shell;
 
 include!("src/app.rs");
 
 fn main() {
-    let outdir = "shell_completion";
+    let outdir = match std::env::var_os("OUT_DIR") {
+        Some(path) => PathBuf::from(path),
+        None => PathBuf::from("."),
+    };
+    let outdir = outdir.join("shell_completion");
+
+    std::fs::create_dir_all(&outdir).unwrap();
+
     let mut app = build();
     app.gen_completions("ff", Shell::Bash, &outdir);
     app.gen_completions("ff", Shell::Fish, &outdir);

7d7ec2ae8da1b0794361a01b96f730b89128f486
J.W <jakwings@gmail.com>
2019-03-28 07:58:20 +0800

    Combine sorter thread and receiver thread

--- src/walk.rs
+++ src/walk.rs
@@ -76,170 +76,77 @@ fn calc_recv_threads(threads: usize, sort_path: bool) -> usize {
     }
 }
 
-fn spawn_receiver_thread(
+fn spawn_receiver_threads(
     rx: mpsc::Receiver<PathBuf>,
     config: Arc<AppOptions>,
     quitting: Arc<AtomicUsize>,
-) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        let mut rx_counter = Counter::new(MAX_CNT, Some(Arc::clone(&quitting)));
+) -> Vec<thread::JoinHandle<()>> {
+    // This will be set to `Some` if the `--exec` argument was supplied.
+    if let Some(ref cmd) = config.command {
+        // Broadcast the stdin input to all child processes.
+        let cached_input = if config.multiplex {
+            let mut rx_counter = Counter::new(MAX_CNT, Some(Arc::clone(&quitting)));
+            let stdin = io::stdin();
+            let fdin = stdin.as_raw_fd();
+            let mut lock = stdin.lock();
+            let mut bytes = Vec::new();
+            // Do not allow blocking I/O to delay the shutdown of this program.
+            // e.g. when waiting for user input.
+            let aborted =
+                match exec::select_read_to_end(&mut rx_counter, fdin, &mut lock, &mut bytes) {
+                    Ok(None) => true,
+                    Ok(Some(_size)) => false,
+                    Err(err) => fatal(&err.to_string()),
+                };
 
-        // This will be set to `Some` if the `--exec` argument was supplied.
-        if let Some(ref cmd) = config.command {
-            // Broadcast the stdin input to all child processes.
-            let cached_input = if config.multiplex {
-                let stdin = io::stdin();
-                let fdin = stdin.as_raw_fd();
-                let mut lock = stdin.lock();
-                let mut bytes = Vec::new();
-                // Do not allow blocking I/O to delay the shutdown of this program.
-                // e.g. when waiting for user input.
-                let aborted =
-                    match exec::select_read_to_end(&mut rx_counter, fdin, &mut lock, &mut bytes) {
-                        Ok(None) => true,
-                        Ok(Some(_size)) => false,
-                        Err(err) => fatal(&err.to_string()),
-                    };
+            drop(lock);
 
-                drop(lock);
-
-                if aborted {
-                    error("receiver thread failed to read from stdin");
-                    return;
-                } else {
-                    Some(bytes)
-                }
+            if aborted {
+                fatal("receiver thread failed to read from stdin");
             } else {
-                None
-            };
-
-            let threads = calc_recv_threads(config.threads, config.sort_path);
-            let cmd = Arc::new(cmd.clone());
-            // Enable caching for broadcast, as interactive input may not satisfy all commands.
-            let input = Arc::new(cached_input);
-            // It is unsafe to interact with mixed output from different commands.
-            let no_stdin = threads > 1 && atty::is(atty::Stream::Stdin);
-            // Reorder the output only when necessary.
-            let cache_output = threads > 1;
-
-            let rx = Arc::new(Mutex::new(rx));
-            let mut handles = Vec::with_capacity(threads);
-
-            for _ in 0..threads {
-                let rx = Arc::clone(&rx);
-                let cmd = Arc::clone(&cmd);
-                let input = Arc::clone(&input);
-                let quitting = Arc::clone(&quitting);
-                let mut counter = Counter::new(MAX_CNT / threads, Some(quitting));
-                let handle = thread::spawn(move || {
-                    exec::schedule(counter, rx, cmd, input, no_stdin, cache_output);
-                });
-
-                handles.push(handle);
-            }
-
-            // Wait for all threads to exit before exiting the program.
-            for h in handles {
-                h.join()
-                    .unwrap_or_else(|_| fatal("unable to process search results"));
+                Some(bytes)
             }
         } else {
-            for value in rx {
-                if rx_counter.inc() {
-                    error("receiver thread aborted");
-                    return;
-                }
-                output::print_entry(&value, &config);
-            }
-        }
-    })
-}
-
-fn spawn_sorter_thread(
-    xtx: mpsc::Sender<PathBuf>,
-    rx: mpsc::Receiver<PathBuf>,
-    config: Arc<AppOptions>,
-) -> thread::JoinHandle<()> {
-    let tx = xtx.clone();
-    let sorter_thread = thread::spawn(move || {
-        let max_buffer_time = if atty::is(atty::Stream::Stdout) {
-            config.max_buffer_time.unwrap_or(100)
-        } else {
-            0
+            None
         };
 
         let threads = calc_recv_threads(config.threads, config.sort_path);
-        let mut buffer = Vec::new();
-        let mut mode = if config.sort_path {
-            ReceiverMode::Buffering(BufferTime::Eternity)
-        } else if max_buffer_time > 0 && (config.command.is_none() || threads == 1) {
-            ReceiverMode::Buffering(BufferTime::Duration)
-        } else {
-            ReceiverMode::Streaming
-        };
 
-        let mut counter = Counter::new(MAX_CNT, None);
-        let start = time::Instant::now();
-        let duration = time::Duration::from_millis(max_buffer_time);
+        let cmd = Arc::new(cmd.clone());
+        // Enable caching for broadcast, as interactive input may not satisfy all commands.
+        let input = Arc::new(cached_input);
+        // It is unsafe to interact with mixed output from different commands.
+        let no_stdin = threads > 1 && atty::is(atty::Stream::Stdin);
+        // Reorder the output only when necessary.
+        let cache_output = threads > 1;
 
-        for value in rx {
-            match mode {
-                ReceiverMode::Buffering(buf_time) => match buf_time {
-                    BufferTime::Duration => {
-                        buffer.push(value);
+        let mut handles = Vec::with_capacity(threads);
+        let rx = Arc::new(Mutex::new(rx));
 
-                        if counter.inc() && time::Instant::now() - start > duration {
-                            for v in buffer.drain(0..) {
-                                if tx.send(v).is_err() {
-                                    error("sorter thread failed to send data");
-                                    return;
-                                }
-                            }
-                            mode = ReceiverMode::Streaming;
-                        }
-                    }
-                    BufferTime::Eternity => {
-                        buffer.push(value);
-                    }
-                },
-                ReceiverMode::Streaming => {
-                    if tx.send(value).is_err() {
-                        error("sorter thread failed to send data");
-                        return;
-                    }
-                }
-            }
+        for _ in 0..threads {
+            let rx = Arc::clone(&rx);
+            let cmd = Arc::clone(&cmd);
+            let input = Arc::clone(&input);
+            let quitting = Arc::clone(&quitting);
+            let mut counter = Counter::new(MAX_CNT / threads, Some(quitting));
+            let handle = thread::spawn(move || {
+                exec::schedule(counter, rx, cmd, input, no_stdin, cache_output);
+            });
+
+            handles.push(handle);
         }
 
-        if !buffer.is_empty() {
-            // Stable sort is fast enough for nearly sorted items,
-            // although it uses 50% more memory than unstable sort.
-            // Would parallel sort really help much? Skeptical.
-            buffer.sort();
+        return handles;
+    }
 
-            for value in buffer {
-                if tx.send(value).is_err() {
-                    error("sorter thread failed to send data");
-                    return;
-                }
-            }
-        }
-    });
-
-    drop(xtx);
-
-    sorter_thread
+    Vec::new()
 }
 
 fn spawn_sender_threads(
     tx: mpsc::Sender<PathBuf>,
     config: Arc<AppOptions>,
     quitting: Arc<AtomicUsize>,
-) -> thread::JoinHandle<()> {
-    // middleware for sorting
-    let (xtx, xrx) = mpsc::channel();
-    let sorter_thread = spawn_sorter_thread(tx, xrx, Arc::clone(&config));
-
+) {
     let threads = calc_send_threads(config.threads, config.sort_path);
     let walker = WalkBuilder::new(&config.root)
         .hidden(!config.dot_files)
@@ -258,7 +165,7 @@ fn spawn_sender_threads(
 
     // Spawn the sender threads.
     walker.run(|| {
-        let tx = xtx.clone();
+        let tx = tx.clone();
         let config = Arc::clone(&config);
         let quitting = Arc::clone(&quitting);
         let mut tx_counter = Counter::new(MAX_CNT, Some(quitting));
@@ -389,9 +296,117 @@ fn spawn_sender_threads(
 
     // Drop the sender. If we don't do this, the receiver will block even
     // if all threads have finished, since there is still one sender around.
-    drop(xtx);
+    drop(tx);
+}
 
-    sorter_thread
+#[inline(always)]
+fn print_or_pipe(
+    print_mode: bool,
+    path: PathBuf,
+    tx: &mpsc::Sender<PathBuf>,
+    config: &Arc<AppOptions>,
+) -> bool {
+    if print_mode {
+        output::print_entry(&path, config);
+    } else {
+        if tx.send(path).is_err() {
+            error("sorter thread failed to send data");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn transfer_data(
+    handles: Vec<thread::JoinHandle<()>>,
+    tx: mpsc::Sender<PathBuf>,
+    rx: mpsc::Receiver<PathBuf>,
+    config: Arc<AppOptions>,
+    quitting: Arc<AtomicUsize>,
+) -> i32 {
+    let exitcode = 0;
+    let print_mode = handles.is_empty();
+
+    let max_buffer_time = if atty::is(atty::Stream::Stdout) {
+        config.max_buffer_time.unwrap_or(100)
+    } else {
+        0
+    };
+
+    let threads = calc_recv_threads(config.threads, config.sort_path);
+    let mut buffer = Vec::new();
+    let mut mode = if config.sort_path {
+        ReceiverMode::Buffering(BufferTime::Eternity)
+    } else if max_buffer_time > 0 && (config.command.is_none() || threads == 1) {
+        ReceiverMode::Buffering(BufferTime::Duration)
+    } else {
+        ReceiverMode::Streaming
+    };
+
+    let mut rx_counter = Counter::new(MAX_CNT, Some(Arc::clone(&quitting)));
+    let mut counter = Counter::new(MAX_CNT, None);
+    let start = time::Instant::now();
+    let duration = time::Duration::from_millis(max_buffer_time);
+
+    for value in rx {
+        if rx_counter.inc() {
+            error("sorter thread aborted");
+            return 1;
+        }
+        match mode {
+            ReceiverMode::Buffering(buf_time) => match buf_time {
+                BufferTime::Duration => {
+                    buffer.push(value);
+
+                    if counter.inc() && time::Instant::now() - start > duration {
+                        for value in buffer.drain(0..) {
+                            if !print_or_pipe(print_mode, value, &tx, &config) {
+                                return 1;
+                            }
+                        }
+                        mode = ReceiverMode::Streaming;
+                    }
+                }
+                BufferTime::Eternity => {
+                    buffer.push(value);
+                }
+            },
+            ReceiverMode::Streaming => {
+                if !print_or_pipe(print_mode, value, &tx, &config) {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    if !buffer.is_empty() {
+        // Stable sort is fast enough for nearly sorted items,
+        // although it uses 50% more memory than unstable sort.
+        // Would parallel sort really help much? Skeptical.
+        buffer.sort();
+
+        for value in buffer {
+            if rx_counter.inc() {
+                error("sorter thread aborted");
+                return 1;
+            }
+            if !print_or_pipe(print_mode, value, &tx, &config) {
+                return 1;
+            }
+        }
+    }
+
+    drop(tx);
+
+    // Wait for the --exec threads to print out all results.
+    for handle in handles {
+        if handle.join().is_err() {
+            fatal("failed to process search results");
+        }
+    }
+
+    exitcode
 }
 
 // Recursively scan the given search path for files/pathnames matching the pattern.
@@ -400,8 +415,6 @@ fn spawn_sender_threads(
 // jobs in parallel from a given command line and the discovered paths. Otherwise, each
 // path will simply be written to standard output.
 pub fn scan(config: Arc<AppOptions>) {
-    let (tx, rx) = mpsc::channel();
-
     // A signal to tell the colorizer or the command processor to exit gracefully.
     let quitting = Arc::new(AtomicUsize::new(0));
 
@@ -419,19 +432,19 @@ pub fn scan(config: Arc<AppOptions>) {
     )
     .unwrap_or_else(|_| fatal("could not set SIGTERM handler"));
 
-    // Spawn the threads that receive or send results through the channel.
-    let recv_handle = spawn_receiver_thread(rx, Arc::clone(&config), Arc::clone(&quitting));
-    let send_handle = spawn_sender_threads(tx, Arc::clone(&config), Arc::clone(&quitting));
+    let (tx, rx) = mpsc::channel();
+    // middleware for sorting
+    let (xtx, xrx) = mpsc::channel();
 
-    // Wait for the sender thread to sort & send all results.
-    send_handle
-        .join()
-        .unwrap_or_else(|_| fatal("unable to produce search results"));
+    let handles = spawn_receiver_threads(xrx, Arc::clone(&config), Arc::clone(&quitting));
 
-    // Wait for the receiver thread to print out all results.
-    recv_handle
-        .join()
-        .unwrap_or_else(|_| fatal("unable to collect search results"));
+    spawn_sender_threads(tx, Arc::clone(&config), Arc::clone(&quitting));
+
+    let exitcode = transfer_data(handles, xtx, rx, Arc::clone(&config), Arc::clone(&quitting));
 
     exit_if_sigint(&quitting);
+
+    if exitcode != 0 {
+        exit(exitcode);
+    }
 }
